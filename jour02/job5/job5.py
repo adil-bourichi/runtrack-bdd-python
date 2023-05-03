9141ae26fd713bdfa77830d9f7449a4f9ee22e77
adil-bourichi <adil.bourichi@laplateforme.io>
2023-05-03 15:24:12 +0200

    job5

---
+++ jour02/job5/job5.py
@@ -0,0 +1,20 @@
+import mysql.connector
+
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Zen4321@",
+    database="LaPlateforme"
+)
+
+
+cursor = db.cursor()
+
+
+query = "SELECT SUM(superficie) AS total_superficie FROM etage;"
+cursor.execute(query)
+
+result = cursor.fetchone()[0]
+
+print("La superficie de La Plateforme est de", result, "m2")
+
